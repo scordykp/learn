15669effb4e16f33677898ffc4b2d729f6ce71ff
scordykp <scordykp@ioiio.cc>
2025-11-11 17:34:40 +0700

    up

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <vector>
 
-int main() {
-  std::cout << "Test" << "\n";
+int main()
+{
+  std::cout << "4.5 - Unsigned integers" << "\n";
+
+  unsigned short us;
+  unsigned int ui;
+  unsigned long ul;
+  unsigned long long ull;
+
+  std::cout << "size unsigned short: " << sizeof(us) << "\n";
+  std::cout << "size unsigned interger: " << sizeof(ui) << "\n";
+  std::cout << "size unsigned long: " << sizeof(ul) << "\n";
+  std::cout << "size unsigned long long: " << sizeof(ull) << "\n";
 
   return 0;
 }
